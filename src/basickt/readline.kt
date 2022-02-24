db72fa916638a2cf1c15efeab01b184874b0917d
Murillo Alves da Silva <72824080+tumusx@users.noreply.github.com>
2022-02-23 23:07:01 -0300

    create file condicionais

--- src/basickt/readline.kt
+++ src/basickt/readline.kt
@@ -15,6 +15,34 @@ private fun checkGeometric() {
     }
 }
 
+private fun checkGeometricRectangle() {
+    val number1: String? = readLine()
+    val number2: String? = readLine()
+    val number3: String? = readLine()
+
+    number1?.toInt()
+    number2?.toInt()
+    number3?.toInt()
+
+    if (number1 == number2 && number1 == number3 && number2 == number3) {
+        println("eh um triangulo equilatero")
+    } else {
+        println("nao eh um triangulo equilatero")
+    }
+}
+
+private fun checkAge() {
+    val idade: String = readLine().toString()
+
+    if (idade < "18") {
+        println("voce nao pode entrar")
+    } else {
+        println("documentos verificados")
+    }
+}
+
 fun main() {
+    checkAge()
     checkGeometric()
+    checkGeometricRectangle()
 }
